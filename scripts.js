42b1e59b1932e806f4bc9409cf1dd98d8763c55c
MungaiKeren <wambukeren@gmail.com>
2019-07-27 22:47:19 +0300

    This commit will add comments to code

--- scripts.js
+++ scripts.js
@@ -5,19 +5,25 @@ function order(size, crust, toppings, total) {
     this.toppings = toppings;
     this.total = total;
 }
-
+//displays total order for pizza selected
+//also displays the delivery option
 $(document).ready(function(){
     $('button#order').click(function(){
         alert("Your total order is:")
         $('.delivery').show();
     });
 });
+//displays total for order added
+$(document).ready(function(){
+
+});
+//displays form for delivery
 $(document).ready(function(){
     $('button#deliver').click(function(){
         $('.location').show();
     });
 });
-
+//displays an alert for delivery location in accordance to input
 $(document).ready(function(){
     $('form#place').submit(function(){
         var building = $('input#building').val();
