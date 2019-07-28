bbb246ac32f7b6968d90714ac2e19f99f65b54ce
MungaiKeren <wambukeren@gmail.com>
2019-07-28 14:12:28 +0300

    id form fields

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
 var size, crust, toppings, total;
-function order(size, crust, toppings, total) {
+function Pizza(size, crust, toppings, total) {
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
     this.total = total;
 }
+
 //displays total order for pizza selected
 //also displays the delivery option
 $(document).ready(function(){
     $('button#order').click(function(){
+        var pizzaSize1 = $('#large option:selected').val();
+
         alert("Your total order is:")
         $('.delivery').show();
     });
 });
 //displays total for order added
 $(document).ready(function(){
-
+    $('button#final').click(function(){
+        alert("you have "+"_ "+"orders.");
+    });
 });
 //displays form for delivery
 $(document).ready(function(){
